dc2d4e19bc60f5aaa2af9ffe8929eaa0297f5e76
Jerry <mejerrytech@gmail.com>
2024-08-21 19:53:52 +0530

    Feat(Auth): Implemented the keycloak in the login and signup functionality

--- src/app.d.ts
+++ src/app.d.ts
@@ -2,34 +2,34 @@ import { SupabaseClient, Session } from '@supabase/supabase-js'
 import { Database } from './DatabaseDefinitions'
 import keycloak from '$lib/aws/auth'
 
-declare global {
-	namespace App {
-		interface Locals {
-			supabase: SupabaseClient<Database>
-			getSession(): Promise<Session | null>
-		}
-		interface PageData {
-			session: Session | null
-		}
-		// interface Error {}
-		// interface Platform {}
-	}
-}
-
 // declare global {
 // 	namespace App {
-// 	  interface Locals {
-// 		keycloak: KeycloakInstance;
-// 		getSession(): Promise<{
-// 		  token: string;
-// 		  userInfo: any; 
-// 		} | null>;
-// 	  }
-// 	  interface PageData {
-// 		session: {
-// 		  token: string;
-// 		  userInfo: any; 
-// 		} | null;
-// 	  }
+// 		interface Locals {
+// 			supabase: SupabaseClient<Database>
+// 			getSession(): Promise<Session | null>
+// 		}
+// 		interface PageData {
+// 			session: Session | null
+// 		}
+// 		// interface Error {}
+// 		// interface Platform {}
 // 	}
-//   }
+// }
+
+declare global {
+	namespace App {
+	  interface Locals {
+		keycloak: KeycloakInstance;
+		getSession(): Promise<{
+		  token: string;
+		  userInfo: any; 
+		} | null>;
+	  }
+	  interface PageData {
+		session: {
+		  token: string;
+		  userInfo: any; 
+		} | null;
+	  }
+	}
+  }
--- src/routes/auth/+page.server.js
+++ src/routes/auth/+page.server.js
@@ -9,11 +9,11 @@ export async function load({ url, parent }) {
   const joining_server = url.pathname.includes('set-password')
 
   if (!session && !signing_up && !joining_server) {
-    const query = 'SELECT * FROM users'
+    const query = 'SELECT COUNT(*) FROM users'
     const existing_users = await pool.query(query)
-    console.log('result1 : ', existing_users)
 
-    const initiated = existing_users?.length > 0
+    const initiated = existing_users.rows[0].count > 0
+    console.log('initiated : ', initiated)
 
     if (!initiated) {
       throw redirect(303, '?signup')
@@ -30,8 +30,6 @@ export const actions = {
     const data = await request.formData()
     const email = data.get('email')
     const password = data.get('password')
-    console.log('email : ', email)
-    console.log('password : ', password)
 
     // Define Keycloak configuration
     const keycloakServerUrl =
@@ -68,6 +66,72 @@ export const actions = {
 
     // Request a token from Keycloak
     try {
+      const logInResponse = await fetch(keycloakServerUrl, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        body: body,
+      })
+
+      const response = await logInResponse.json()
+
+      if (logInResponse.ok) {
+        return {
+          success: true,
+          error: null,
+          token: response.access_token,
+        }
+      } else {
+        return {
+          success: false,
+          error: response.error_description || 'Failed to login the user',
+        }
+      }
+    } catch (error) {
+      return {
+        success: false,
+        error: 'Unexpected error occurred while login the user.',
+      }
+    }
+  },
+  sign_up: async ({ request }) => {
+    // ensure server is provisioned
+    const { success, error } = await server_provisioned()
+    if (!success) {
+      return {
+        success: false,
+        error,
+      }
+    }
+
+    const count = await pool.query('SELECT COUNT(*) AS count FROM users')
+    if (count > 0) {
+      return {
+        success: false,
+        error:
+          'Server already initialized. Sign in as the server owner to invite users.',
+      }
+    }
+
+    const data = await request.formData()
+    const email = data.get('email')
+    const username = data.get('email')
+    const password = data.get('password')
+
+    const keycloakServerUrl =
+      'http://localhost:8080/realms/master/protocol/openid-connect/token'
+    const clientId = 'admin-client'
+    const clientSecret = 'aJq58lU50Bmx8KtnHKv22HrznZZk04rG'
+
+    // Prepare request body using URLSearchParams
+    const body = new URLSearchParams({
+      grant_type: 'client_credentials',
+      client_id: clientId,
+      client_secret: clientSecret,
+    }).toString()
+
+    try {
       const tokenResponse = await fetch(keycloakServerUrl, {
         method: 'POST',
         headers: {
@@ -79,13 +143,84 @@ export const actions = {
       const tokenData = await tokenResponse.json()
 
       if (tokenResponse.ok) {
-        // Token retrieval successful
-        console.log('Keycloak Token:', tokenData)
-        return {
-          success: true,
-          error: null,
-          token: tokenData.access_token, // Returning the token or processing further
+        const keycloakCreateUserUrl =
+          'http://localhost:8080/admin/realms/master/users'
+        var userId
+
+        try {
+          const responseCreateUser = await fetch(keycloakCreateUserUrl, {
+            method: 'POST',
+            headers: {
+              Authorization: `Bearer ${tokenData.access_token}`,
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+              username: username,
+              email: email,
+              emailVerified: 'true',
+              enabled: true,
+            }),
+          })
+
+          if (!responseCreateUser.ok) {
+            const error = await responseCreateUser.text()
+            throw new Error(
+              `Error creating user: ${responseCreateUser.statusText} (${responseCreateUser.status}) - ${error}`
+            )
+          }
+          const locationHeader = responseCreateUser.headers.get('location')
+          if (locationHeader) {
+            userId = locationHeader.split('/').pop() // Extract the user ID from the URL
+          } else {
+            console.error('Location header not found in the response.')
+          }
+          console.log('User created successfully')
+        } catch (error) {
+          console.error('Failed to create user:', error)
         }
+
+        const keycloakAddPasswordUrl = `http://localhost:8080//admin/realms/master/users/${userId}/reset-password`
+        const responseAddPassword = await fetch(keycloakAddPasswordUrl, {
+          method: 'PUT',
+          headers: {
+            Authorization: `Bearer ${tokenData.access_token}`,
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            type: 'password',
+            value: password,
+            temporary: false,
+          }),
+        })
+
+        // Prepare request body using URLSearchParams
+        const body = new URLSearchParams({
+          grant_type: 'password',
+          client_id: clientId,
+          client_secret: clientSecret,
+          scope: 'openid',
+          username: email.toString(), // Ensure the values are strings
+          password: password.toString(),
+        }).toString()
+
+        const logInResponse = await fetch(keycloakServerUrl, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+          },
+          body: body,
+        })
+
+        console.log('tokenResponse-login-after-signup : ', logInResponse)
+
+        const query = 'INSERT INTO users (id,email) VALUES($1, $2)'
+        const result = await pool.query(query, [userId, email])
+
+        // add user to server_members as admin
+        const query1 =
+          'INSERT INTO server_members ("user",role,admin) VALUES($1, $2, $3)'
+        const result1 = await pool.query(query1, [userId, 'DEV', true])
+        console.log('result5 : ', result1)
       } else {
         // Token retrieval failed
         console.error('Keycloak token retrieval failed:', tokenData)
@@ -105,153 +240,51 @@ export const actions = {
       }
     }
   },
-  sign_up: async ({ request, locals: { supabase } }) => {
-    // Only the server owner signs up, all others are invited (i.e. auto-signed up)
-
-    // ensure server is provisioned
-    const { success, error } = await server_provisioned()
-    if (!success) {
-      return {
-        success: false,
-        error,
-      }
-    }
-
-    const count = await supabase_admin
-      .from('users')
-      .select('count')
-      .then(({ data }) => data?.[0]['count'])
-
-    const result = await pool.query('SELECT COUNT(*) AS count FROM users')
-    console.log('result3 : ', result)
-    if (count > 0) {
-      return {
-        success: false,
-        error:
-          'Server already initialized. Sign in as the server owner to invite users.',
-      }
-    }
-
-    const data = await request.formData()
-    const email = data.get('email')
-    const password = data.get('password')
-
-    const { data: res, error: auth_error } =
-      await supabase_admin.auth.admin.createUser({
-        // @ts-ignore
-        email: email,
-        // @ts-ignore
-        password: password,
-        email_confirm: true,
-      })
-
-    if (auth_error) {
-      return {
-        success: false,
-        error: auth_error.message,
-      }
-    } else if (res) {
-      // @ts-ignore
-      const { error: signin_error } = await supabase.auth.signInWithPassword({
-        email,
-        password,
-      })
-
-      if (!signin_error) {
-        const query = 'INSERT INTO users (id,email) VALUES($1, $2)'
-        const result = await pool.query(query, [res.user?.id, res.user?.email])
-        console.log('result4 : ', result)
-        // disable email confirmation and add user
-        await supabase_admin.from('users').insert({
-          id: res.user?.id,
-          email: res.user?.email,
-        })
-
-        // add user to server_members as admin
-        const query1 =
-          'INSERT INTO server_members (user,role,admin) VALUES($1, $2, $3)'
-        const result1 = await pool.query(query1, [res.user?.id, 'DEV', true])
-        console.log('result5 : ', result1)
-        await supabase_admin.from('server_members').insert({
-          user: res.user?.id,
-          role: 'DEV',
-          admin: true,
-        })
-      }
-
-      return {
-        success: !signin_error,
-        error: signin_error?.message,
-      }
-    }
-  },
 }
 
 async function server_provisioned() {
-  const { status, error } = await supabase_admin.from('sites').select()
-  if (status === 0) {
+  try {
+    // Connect to the database
+    const client = await pool.connect()
+
+    try {
+      // Execute the query to select all data from the 'sites' table
+      const result = await client.query('SELECT * FROM sites')
+
+      // Retrieve data and status
+      const data = result.rows
+      const statusCode = result.rowCount > 0 ? 200 : 204
+
+      if (statusCode === 204) {
+        return {
+          success: false,
+          error: 'No sites found in the database.',
+        }
+      } else if (statusCode === 200) {
+        // Data exists or no data
+        return { success: true, error: null }
+      } else {
+        return { success: false, error: 'Unknown error' }
+      }
+    } catch (err) {
+      // Handle query execution error
+      console.error('Error executing query:', err)
+      return {
+        success: false,
+        error:
+          'Query execution failed. Ensure your database schema is correctly set up.',
+      }
+    } finally {
+      // Release the client back to the pool
+      client.release()
+    }
+  } catch (err) {
+    // Handle connection error
+    console.error('Error connecting to the database:', err)
     return {
       success: false,
-      error: `Could not connect your Supabase backend. Ensure you've correctly connected your environment variables.`,
+      error:
+        'Could not connect to the database. Check your connection configuration.',
     }
-  } else if (status === 404) {
-    console.error(error)
-    return {
-      success: false,
-      error: `Your Supabase backend is connected but incorrectly provisioned. Ensure you've run the setup schema outlined in the Docs.`,
-    }
-  } else if (status === 200) {
-    // has sites or no sites
-    return { success: true, error: null }
-  } else {
-    return { success: false, error: `Unknown error` }
   }
 }
-
-// Please un comment if you want to Use the below function when we go with the Postgresql.
-// async function server_provisioned() {
-//   try {
-//     // Connect to the database
-//     const client = await pool.connect()
-
-//     try {
-//       // Execute the query to select all data from the 'sites' table
-//       const result = await client.query('SELECT * FROM sites')
-
-//       // Retrieve data and status
-//       const data = result.rows
-//       const statusCode = result.rowCount > 0 ? 200 : 204
-
-//       if (statusCode === 204) {
-//         return {
-//           success: false,
-//           error: 'No sites found in the database.',
-//         }
-//       } else if (statusCode === 200) {
-//         // Data exists or no data
-//         return { success: true, error: null }
-//       } else {
-//         return { success: false, error: 'Unknown error' }
-//       }
-//     } catch (err) {
-//       // Handle query execution error
-//       console.error('Error executing query:', err)
-//       return {
-//         success: false,
-//         error:
-//           'Query execution failed. Ensure your database schema is correctly set up.',
-//       }
-//     } finally {
-//       // Release the client back to the pool
-//       client.release()
-//     }
-//   } catch (err) {
-//     // Handle connection error
-//     console.error('Error connecting to the database:', err)
-//     return {
-//       success: false,
-//       error:
-//         'Could not connect to the database. Check your connection configuration.',
-//     }
-//   }
-// }
